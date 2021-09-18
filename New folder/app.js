5a54a8b59c3c275d4298d2c834ca76209b237469
swayam <78816779+Swayam-ops@users.noreply.github.com>
2021-09-18 17:27:00 +0530

    Add files via upload
    <###########>
    json package file

---
+++ New folder/app.js
@@ -0,0 +1,20 @@
+/*var SerialPort = require("serialport");
+const parsers = SerialPort.parsers;
+const parser = new Parser.Readline({
+    delineter: '\r\n'
+})
+
+var port = new SerialPort("COM3 (Arduino Uno)", {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+
+});
+
+port.pipe(parser);
+
+parser.on('data', function(data) {
+    console.log(data);
+})*/
